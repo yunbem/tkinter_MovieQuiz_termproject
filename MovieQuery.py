1997bc95a5c0fdc78234221d1dc1661e8622a059
yunbem <yunbeomgo@gmail.com>
2023-05-25 13:32:53 +0900

    임의의 포스터 출력(중복)

---
+++ MovieQuery.py
@@ -0,0 +1,102 @@
+import requests
+import random
+
+class MovieQuery:
+    def __init__(self):
+        params = {
+            "api_key": "7dd451947ac9f89cc2c61f8dce323beb",
+            "language": "ko",
+            "page": "1",
+            #"sort_by": "popularity.desc",  # 인기순으로 정렬
+            "sort_by": "vote_average.desc",  # 평점순으로 정렬
+            #"include_adult": "false"  # 성인 영화 제외
+            "vote_count.gte": "200",  # 최소 투표 수 조건
+        }
+
+        url = 'https://api.themoviedb.org/3/discover/movie'  # 정보를 요청할 주소
+        resp = requests.get(url, params=params)
+        self.data = resp.json()['results']
+
+        self.bookmarks = []  # 즐겨찾기 저장할 리스트
+
+    def getRandomQuery(self):
+        if self.data:
+            query = random.choice(self.data)  # 영화 목록에서 랜덤하게 영화 선택
+            return query
+        else:
+            print("data를 받아오지 못했습니다.")
+            return None 
+
+    def getMovieInfo(self, query): # 영화 정보를 받습니다.
+        # 영화 정보 출력
+        print("영화 제목:", query['title'])
+        print("개봉 일자:", query['release_date'])
+        print("평점:", query['vote_average'])
+
+        if 'overview' in query and query['overview']:
+            print("개요:", query['overview'])
+        else:
+            print("개요 정보가 없습니다. 다른 쿼리로 영화 정보를 조회합니다.")
+            new_query = self.getRandomQuery()  # 다른 쿼리를 가져옴
+            self.getMovieInfo(new_query)  # 다른 쿼리로 재귀적으로 호출
+
+    def PosterDownload(self, query, num): # 영화 포스터 이미지를 받습니다
+        if query['poster_path']:
+            poster_path = query['poster_path']
+            poster_url = f"https://image.tmdb.org/t/p/w500/{poster_path}"
+
+            response = requests.get(poster_url)
+
+            if response.status_code == 200:
+                if num == -1:
+                    with open('answer_poster.jpg', 'wb') as f:
+                        f.write(response.content)
+                elif num == 0:
+                    with open('poster0.jpg', 'wb') as f:
+                        f.write(response.content)
+                elif num == 1:
+                    with open('poster1.jpg', 'wb') as f:
+                        f.write(response.content)
+                elif num == 2:
+                    with open('poster2.jpg', 'wb') as f:
+                        f.write(response.content)
+                print("포스터 이미지 다운로드 완료")
+            else:
+                print("포스터 이미지 다운로드 실패")
+
+    def getMovieTitle(self, query):
+        return query['title']
+
+    def getMovieRelease(self, query):
+        return query['release_date']
+
+    def getMovieVote(self, query):
+        return str(query['vote_average'])
+
+    def getMovieOverview(self, query):
+        return query['overview']
+
+    def getSimilarGenreMovies(self, query):
+        genre_id = query['genre_ids'][0] if 'genre_ids' in query else None
+
+        if genre_id:
+            similar_movies = []
+            for movie in self.data:
+                if 'genre_ids' in movie and genre_id in movie['genre_ids']:
+                    similar_movies.append(movie)
+            
+            return similar_movies
+        else:
+            print("영화의 장르 정보가 없습니다.")
+            return None
+
+    def addBookmark(self, query):
+        self.bookmarks.append(query)
+
+    def removeBookmark(self, query):
+        if query in self.bookmarks:
+            self.bookmarks.remove(query)
+
+    def getBookmarks(self):
+        return self.bookmarks
+        
---
+++ MovieQuiz.py
@@ -0,0 +1,120 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import font
+from PIL import ImageTk, Image
+from MovieQuery import *
+
+class MovieQuiz:
+    def show_screen(self, screen_num):  # 화면 전환을 처리하는 함수
+        screens = [self.screen0_frame, self.screen1_frame, self.screen2_frame, self.screen3_frame]
+        for i, screen in enumerate(screens):
+            if i == screen_num:
+                screen.pack()
+            else:
+                screen.pack_forget()
+
+    def setupImageButton(self):  # 이미지 버튼 생성 및 배치
+        button_coordinates = [(20, 20), (20, 220), (20, 420), (20, 620)] # 좌측 프레임 내 버튼 4개
+        for i, (x, y) in enumerate(button_coordinates):
+            image_button = Button(self.left_frame, width=80, height=80, command=lambda num=i: self.show_screen(num))
+            image_button.place(x=x, y=y)
+            self.image_buttons.append(image_button)
+
+    def setupPosterButton(self):  # 포스터 버튼 생성 및 배치
+        button_coordinates = [(40, 340), (240, 340), (40, 560), (240, 560)] # 퀴즈 화면 프레임 내 포스트 버튼 4개
+        for i, (x, y) in enumerate(button_coordinates):
+            poster_button = Button(self.screen0_frame, width=150, height=200)
+            poster_button.place(x=x, y=y)
+            self.poster_buttons.append(poster_button)
+
+    def setImageButton(self):   # 이미지 로드 및 버튼에 이미지 적용
+        image_filenames = ["물음표.png", "별.png", "그래프.png", "영화관.png"]
+        for i, filename in enumerate(image_filenames):
+            image = Image.open(filename)
+            image = image.resize((80, 80))
+            photo = ImageTk.PhotoImage(image)
+            self.image_buttons[i].config(image=photo)
+            self.image_buttons[i].image = photo
+
+    def setPosterButton(self):  # 포스터 이미지 로드 및 버튼에 포스터 이미지 적용
+        image_filenames = ["answer_poster.jpg", "poster0.jpg", "poster1.jpg", "poster2.jpg"]
+        for i, filename in enumerate(image_filenames):
+            image = Image.open(filename)
+            image = image.resize((150, 200))
+            photo = ImageTk.PhotoImage(image)
+            self.poster_buttons[i].config(image=photo)
+            self.poster_buttons[i].image = photo
+
+    def setupLabel(self):
+        label_texts = ["New 퀴즈", "즐겨찾기", "최근정답율", "상영 영화관"]
+        label_coordinates = [(10, 120), (10, 320), (10, 520), (10, 720)]
+        for text, (x, y) in zip(label_texts, label_coordinates):
+            label = Label(self.left_frame, text=text, width=10, height=1, font=self.fontstyle)
+            label.place(x=x, y=y)
+
+    def __init__(self):
+        # tkinter 윈도우 생성
+        self.window = Tk()
+        self.window.title("TEST_GUI")
+        self.window.geometry("600x800")
+        self.window.configure(bg='white')
+        self.fontstyle = font.Font(self.window, size=8, weight='bold', family='Consolas')
+        self.fontstyle = font.Font(self.window, size=14, weight='bold', family='Consolas')
+
+        self.left_frame = Frame(self.window, width=130, height=800) # 좌측 프레임 생성
+        self.left_frame.pack(side="left")
+
+        self.screen0_frame = Frame(self.window, width=470, height=800, bg="cyan")  # new퀴즈 화면
+        self.screen1_frame = Frame(self.window, width=470, height=800, bg="white")  # 즐겨찾기 화면
+        self.screen2_frame = Frame(self.window, width=470, height=800, bg="white")  # 최근 정답율 화면
+        self.screen3_frame = Frame(self.window, width=470, height=800, bg="white")  # 상영 영화관 화면
+
+        self.image_buttons = []
+        self.poster_buttons = []
+        self.setupImageButton()
+        self.setImageButton()
+        self.setupLabel()
+
+        self.movie = MovieQuery()
+        self.movie_query = self.movie.getRandomQuery()
+        self.movie.PosterDownload(self.movie_query, -1) # -1은 정답은 영화 포스터
+        self.movie.getMovieInfo(self.movie_query)
+
+        # 초기 화면 설정
+        self.show_screen(0)
+        
+        # screen0_frame에 텍스트 정보를 담는 박스와 스크롤바 생성
+        self.text_box = Text(self.screen0_frame, width=60, height=15)
+        '''
+        self.scrollbar = Scrollbar(self.screen0_frame)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+
+        # 텍스트 박스와 스크롤바 연결
+        self.text_box.config(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.text_box.yview)
+        '''
+
+        # 텍스트 박스 상단에 배치
+        self.text_box.place(x=20, y=20)
+      
+        self.set_MovieOverview()
+        similar_movies = self.movie.getSimilarGenreMovies(self.movie_query)
+        if similar_movies:
+            for i, movie in enumerate(similar_movies[:3]):
+                self.movie.PosterDownload(movie, i)
+     
+        # 포스터 이미지 버튼 생성
+        self.setupPosterButton()
+        self.setPosterButton()
+
+        self.window.mainloop()
+
+    def set_MovieOverview(self): # 텍스트 박스에 검색 결과 기입
+        
+        self.text_box.delete('1.0', END)  # 기존 내용 삭제
+
+        overview = self.movie.getMovieOverview(self.movie_query)
+        info = f"{overview}\n\n"
+        self.text_box.insert(END, info)
+
+MovieQuiz()
