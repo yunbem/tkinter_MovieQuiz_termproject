690bfe809c9ad8f70bdf3a40c7b41baab4ec441a
yunbem <yunbeomgo@gmail.com>
2023-05-26 00:47:28 +0900

    줄거리가 없는 경우 예외처리 문제 해결

--- MovieQuery.py
+++ MovieQuery.py
@@ -4,7 +4,7 @@ import random
 class MovieQuery:
     def __init__(self):
         self.params = {
-            "api_key": "7dd451947ac9f89cc2c61f8dce323beb",
+            "api_key": "API_KEY",
             "language": "ko",
             "page": "2",
             #"sort_by": "popularity.desc",  # 인기순으로 정렬
